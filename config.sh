47a76d119b2fea3c9e9206759e104cb464ca52fe
wuqianwana <35764738+wuqianwana@users.noreply.github.com>
2025-09-16 08:23:30 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,13 +5,13 @@ mkdir -p /etc/ntripcaster
 mkdir -p /docker
 mkdir -p /docker/ntripcaster
 
-wget -O /etc/ntripcaster/users.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/users.aut
-wget -O /etc/ntripcaster/groups.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/groups.aut
-wget -O /etc/ntripcaster/clientmounts.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/clientmounts.aut
-wget -O /etc/ntripcaster/sourcemounts.aut https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/sourcemounts.aut
-wget -O /etc/ntripcaster/sourcetable.dat https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/sourcetable.dat
-wget -O /etc/ntripcaster/ntripcaster.conf https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/conf/ntripcaster.conf
+wget -O /etc/ntripcaster/users.aut https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/conf/users.aut
+wget -O /etc/ntripcaster/groups.aut https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/conf/groups.aut
+wget -O /etc/ntripcaster/clientmounts.aut https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/conf/clientmounts.aut
+wget -O /etc/ntripcaster/sourcemounts.aut https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/conf/sourcemounts.aut
+wget -O /etc/ntripcaster/sourcetable.dat https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/conf/sourcetable.dat
+wget -O /etc/ntripcaster/ntripcaster.conf https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/conf/ntripcaster.conf
 
-wget -O /docker/ntripcaster/docker-compose.yml https://raw.githubusercontent.com/rinex20/ntripcaster_docker/master/docker-compose.yml
+wget -O /docker/ntripcaster/docker-compose.yml https://raw.githubusercontent.com/wuqianwana/Ntripcaster-docker/master/docker-compose.yml
 
 ls -l /etc/ntripcaster
